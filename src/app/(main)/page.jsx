4e023d86c95b94d8411df060a1ad3b4a8ac880eb
heeeete <mkoui@naver.com>
2024-08-03 03:13:42 +0900

    Update page.jsx

--- src/app/(main)/page.jsx
+++ src/app/(main)/page.jsx
@@ -3,6 +3,7 @@ import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query
 import getRecentProducts from './_lib/getRecentProducts';
 
 export default async function Page() {
+  console.log('HELLLLLLLLLLLLLLLLL');
   const queryClient = new QueryClient();
   try {
     await queryClient.prefetchQuery({ queryKey: ['recentProducts'], queryFn: () => getRecentProducts() });
@@ -11,6 +12,7 @@ export default async function Page() {
     console.error('recent Products prefetchQuery 실행 중 에러 발생:', error);
   }
   const dehydratedstate = dehydrate(queryClient);
+  console.log('LOWWWWWWWWWWWWWWWWWWW');
   return (
     //HydrationBoundary 컴포넌트로 감싸주면 클라이언트 측에서 별도의 hydrate 호출 없이 서버 측에서 직렬화된 데이터를 자동으로 복원하여 사용
     <HydrationBoundary state={dehydratedstate}>
