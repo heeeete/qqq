0ed326817665ceb1587602dfc07a2e9bbe3dc493
heeeete <mkoui@naver.com>
2024-08-03 03:13:12 +0900

    Update getRecentProducts.js

--- src/app/(main)/_lib/getRecentProducts.js
+++ src/app/(main)/_lib/getRecentProducts.js
@@ -1,7 +1,6 @@
 const getRecentProducts = async () => {
   console.log('GETRECENT!!!!!!!!!!!!!!!!!!!!!!!!!');
-  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
-  const res = await fetch(`${baseUrl}/api/products/recent`, {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/products/recent`, {
     next: { tags: ['recentProducts'] },
   });
   if (!res.ok) {
